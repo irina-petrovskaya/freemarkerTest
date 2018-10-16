1e7bb55da74574cab4b8cd039dc1cc929d0e5105
irina petrovskaya <petrovskaya@gmail.com>
2018-10-16 14:20:10 +0300

    format

--- src/main/java/org/test/fmtest/FreemarkerTestApplication.java
+++ src/main/java/org/test/fmtest/FreemarkerTestApplication.java
@@ -2,6 +2,7 @@ package org.test.fmtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * created by Irina
  */
--- src/main/java/org/test/fmtest/ServletInitializer.java
+++ src/main/java/org/test/fmtest/ServletInitializer.java
@@ -9,9 +9,9 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
  */
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(FreemarkerTestApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FreemarkerTestApplication.class);
+    }
 
 }
