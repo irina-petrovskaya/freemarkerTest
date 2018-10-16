4acd302adabc98f26a0a9390c7973e75515e844e
irina petrovskaya <petrovskaya@gmail.com>
2018-10-16 14:38:25 +0300

    FreemarkerTestApplication.java edit

--- src/main/java/org/test/fmtest/FreemarkerTestApplication.java
+++ src/main/java/org/test/fmtest/FreemarkerTestApplication.java
@@ -1,8 +1,10 @@
 package org.test.fmtest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.context.WebApplicationContext;
 
 /**
  * created by Irina
@@ -10,8 +12,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FreemarkerTestApplication implements CommandLineRunner {
 
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
     @Override
     public void run(String... strings) throws Exception {
+
 
     }
 
